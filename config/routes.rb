45c5779cff0e8a9a5c0ea45298ab1edab82517aa
akimurase <mosworldfor@gmail.com>
2020-11-23 15:13:15 +0900

    plansのMVC作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  get 'plans/index'
+  get 'plans/new'
+  get 'plans/create'
+  get 'plans/edit'
+  get 'plans/update'
+  get 'plans/destroy'
   devise_for :clients
   root to: 'tops#index'
   
