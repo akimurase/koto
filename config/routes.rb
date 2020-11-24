c41a5988f2fbe920f1837b281165bec2c9841f87
akimurase <mosworldfor@gmail.com>
2020-11-24 16:26:20 +0900

    申し込みフォームMVC作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,12 @@
 Rails.application.routes.draw do
 
+  get 'events/index'
+  get 'events/new'
+  get 'events/create'
+  get 'events/show'
+  get 'events/edit'
+  get 'events/update'
+  get 'events/destroy'
   get 'itinerarys/index'
   get 'itinerarys/new'
   get 'itinerarys/create'
