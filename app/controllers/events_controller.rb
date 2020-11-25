dd470b80787160d24c6d5baf0dcc5201b42289b8
akimurase <mosworldfor@gmail.com>
2020-11-25 09:57:42 +0900

    アクティブハッシュ導入

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,14 +1,28 @@
 class EventsController < ApplicationController
   def index
+    # @events = Event.all
   end
 
   def new
+    @event = Event.new
+  end
+
+  def confirm
+    @event = Event.new(event_params)
+    render :new if @event.invalid?
   end
 
   def create
+    @event = Event.new(event_params)
+    if params[:back]
+      render :new
+    else pay_event && @event.save!
+      redirect_to @event
+    end
   end
 
   def show
+    @event = Event.find_by(id: params[:id])
   end
 
   def edit
@@ -18,5 +32,11 @@ class EventsController < ApplicationController
   end
 
   def destroy
+  end
+
+  private
+  
+  def event_params
+    params.require(:event).permit(:num_id, :start_time, :plan_id).merge(user_id: current_user.id)  #,:token
   end
 end
--- app/models/event.rb
+++ app/models/event.rb
@@ -5,6 +5,9 @@ class Event < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
   # アクティブハッシュ用rアソシエーション
   belongs_to_active_hash :num
+
+  validates :num_id, :start_time, :user_id, :plan_id, presence: true
+
   #決済機能 (後で詳しく設定する)
   # attr_accessor :token
 
