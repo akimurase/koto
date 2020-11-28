f15da3142a532812f59689e8014007032e4b25c8
akimurase <mosworldfor@gmail.com>
2020-11-28 16:58:26 +0900

    ユーザーとクライアントのdeviseコントローラー作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,17 @@
 Rails.application.routes.draw do
-  devise_for :users
-  devise_for :clients
-  # devise_for :clients, controllers: {
-  #   sessions:      'clients/sessions',
-  #   passwords:     'clients/passwords',
-  #   registrations: 'clients/registrations'
-  # }
-  # devise_for :users, controllers: {
-  #   sessions:      'users/sessions',
-  #   passwords:     'users/passwords',
-  #   registrations: 'users/registrations'
-  # }
+  # devise_for :clients
+  # devise_for :users
+  devise_for :clients, controllers: {
+    sessions:      'clients/sessions',
+    passwords:     'clients/passwords',
+    registrations: 'clients/registrations'
+  }
+  devise_for :users, controllers: {
+    sessions:      'users/sessions',
+    passwords:     'users/passwords',
+    registrations: 'users/registrations'
+  }
+
   root to: 'tops#index'
   resources :events #管理画面用
 
