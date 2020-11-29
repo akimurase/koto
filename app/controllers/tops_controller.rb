1c2070be66c999abbbafb6bdc951a8631604da99
akimurase <mosworldfor@gmail.com>
2020-11-29 13:55:14 +0900

    トップの新規編集の遷移先修正

--- app/controllers/tops_controller.rb
+++ app/controllers/tops_controller.rb
@@ -1,15 +1,17 @@
 class TopsController < ApplicationController
   def index
     @top = Top.find_by(params[:id])
-    @products = Product.all #レンダーで_product.htmlを表示させる為の定義
+    @products = Product.all #top/indexでproductのデータを表示させる為。元々レンダーで_product.htmlを表示させる為の定義だった。
   end
 
   def new
     @top = Top.new
   end
 
-  def show
-    @top = Top.find_by(params[:id])
+  def show#編集用showページ
+    @top = Top.find_by(params[:id]) #トップ動画
+    @products = Product.all #プロダクト画像
+    # @product = Product.find(params[:id]) #もしtop/showでプロダクトの編集をす場合は、idを特定するためfind(params[:id])が必要
   end
 
   def create
