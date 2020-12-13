fd1d9621430c8903ed5186cde7fdcade9dba1cdd
akimurase <mosworldfor@gmail.com>
2020-12-13 15:47:26 +0900

    申し込みフォームのフロント実装

--- app/controllers/tops_controller.rb
+++ app/controllers/tops_controller.rb
@@ -1,6 +1,7 @@
 class TopsController < ApplicationController
   def index
-    @top = Top.find(current_client.id)
+    # @top = Top.find(current_client.id)
+    @top = Top.find_by(params[:id])
     @products = Product.all #top/indexでproductのデータを表示させる為。元々レンダーで_product.htmlを表示させる為の定義だった。
   end
 
