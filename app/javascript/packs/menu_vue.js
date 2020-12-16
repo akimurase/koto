885a7a54d2df821c283b4e766f46c59737744a62
akimurase <mosworldfor@gmail.com>
2020-12-16 16:45:39 +0900

    非同期エディター実装

--- app/javascript/packs/menu_vue.js
+++ app/javascript/packs/menu_vue.js
@@ -4,3 +4,24 @@ const app = new Vue({
     ActiveBtn: false
   },
 })
+
+const app1 = new Vue({
+  el: '#app1',
+  data: {
+    ActiveBtn: false
+  },
+})
+
+const app2 = new Vue({
+  el: '#app2',
+  data: {
+    ActiveBtn: false
+  },
+})
+
+const app3 = new Vue({
+  el: '#app3',
+  data: {
+    ActiveBtn: false
+  },
+})
